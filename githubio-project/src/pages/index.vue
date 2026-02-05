46454f32b8d668fde40b2e0e7110e6b21a52e510
winpuchit <winpandahaha@gmail.com>
2026-02-05 23:38:35 +0700

    Create Vuetify project

---
+++ githubio-project/src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mb-8 text-center">
+    <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+    <h1 class="text-h2 my-0 font-weight-bold">...</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
